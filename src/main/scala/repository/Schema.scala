93372f6e79bf2b9057d992638c7e45347f4862a3
Rafael Santos <rafael.santos@statflo.com>
2021-12-04 22:24:07 -0500

    Report failed emails

---
+++ src/main/scala/repository/Schema.scala
@@ -0,0 +1,19 @@
+package graphqlzero
+
+def str(v: Option[Any]) =
+  v.orElse(Some("None")).get
+
+def str(v: Int|String) =
+  String.valueOf(v)
+
+case class User(id: Int, name: String, email: Option[String]) {
+  override def toString(): String =
+    s"id: ${str(id)}, name: ${str(name)}, email: ${str(email)}"
+}
+
+case class Email(address:String, body: String) {
+  override def toString: String =
+    s"address: ${str(address)}, body: ${str(body)}"
+}
+
+case class GenericError(message: String)
