53b0e0f6ed354782920cb70abe6a6d8b388dbe0a
Rafael Santos <rafael.santos@statflo.com>
2021-12-05 15:44:23 -0500

    Chosmetic changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,20 +17,43 @@ def sendEmails(users: List[User]): List[Either[GenericError, Email]] =
   println("\n-- All users:")
   printUsers(users)
 
-  val canadianUsers = filterCanadianUsers(users)
+
   println("\n-- Users with Canadian emails:")
+  val canadianUsers = filterCanadianUsers(users)
   printUsers(canadianUsers)
+
 
   println("\n-- Sending welcome emails to Canadian emails:")
   val emailsSent: List[Either[GenericError, Email]] = sendEmails(canadianUsers)
+  emailsSent.foreach(println)
 
-  println("\n---- Emails sent successfully")
-  emailsSent.filter(e => e.isRight).map(e => e.getOrElse("None")).foreach(println)
 
-  println("\n---- Emails that failed to send")
-  emailsSent.filter(e => e.isLeft).map(e => e.left.getOrElse("None")).foreach(println)
+  println("\n---- REPORTING -----")
+  println("-" * 20)
 
-  println("\n---- The same logic in a single stream")
+
+  println("\n-- Report a for loop")
+  for (i <- canadianUsers.indices)
+    val user = canadianUsers(i)
+    val status = emailsSent(i) match {
+      case Right(r) => "Sent"
+      case Left(l) => s"Failed: $l"
+    }
+    println(s"name: ${user.name} \temail: ${user.email.getOrElse("None")} \tstatus: $status")
+
+
+  println("\n-- Report using zip()")
+  canadianUsers.zip(emailsSent)
+    .foreach((user, emailSent) => {
+      val status = emailSent match {
+        case Right(r) => "Sent"
+        case Left(l) => s"Failed: $l"
+      }
+      println(s"name: ${user.name} \temail: ${user.email.getOrElse("None")} \tstatus: $status")
+    })
+
+
+  println("\n---- Redo the entire logic with a single stream")
   val summary = Service
     .queryUsers
     .filter(_.email.getOrElse("").endsWith(".ca"))
